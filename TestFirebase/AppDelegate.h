1f28e3567f5e0153ba46976287f48caec59bb0fe
Sergey Dolin <sergey@s4y.solutions>
2019-03-11 02:12:30 +0500

    Initial Commit

---
+++ TestFirebase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFirebase
+//
+//  Created by Sergey Dolin on 11/03/2019.
+//  Copyright © 2019 Sergey Dolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestFirebase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFirebase
+//
+//  Created by Sergey Dolin on 11/03/2019.
+//  Copyright © 2019 Sergey Dolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
